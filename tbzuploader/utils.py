9865db7b111a4a99ed19c3c53ac268be5fd129db
Thomas Güttler <tguettler@tbz-pariv.de>
2017-10-24 10:15:53 +0200

    log url

--- tbzuploader/utils.py
+++ tbzuploader/utils.py
@@ -36,7 +36,7 @@ def upload_list_of_pairs__single(directory, url, pairs, done_directory, verify):
     if response.status_code == 201:
         return upload_list_of_pairs__single__success(directory, url, pairs, done_directory, response)
     print('Failed: %s' % pairs)
-    print(response)
+    print('%s %s' % (response, url)) # TODO remove password from URL. See https://stackoverflow.com/questions/46905367/remove-password-from-url
     print(response.content)
 
 
