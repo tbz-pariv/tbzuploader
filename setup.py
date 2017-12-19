58d26c5cb85294da6282c21f120d43b608930e3a
Bumpversion after CI <bumpversion-after-ci@example.com>
2017-12-19 15:04:33 +0000

    Bump version: 2017.23.0 -> 2017.24.0 [ci skip]

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='tbzuploader',
-    version='2017.23.0',
+    version='2017.24.0',
     license='Apache Software License 2.0',
     long_description=open('README.rst').read(),
     url='https://github.com/guettli/tbzuploader',
