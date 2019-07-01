ed05c5c00781807849dba1d9a0d21bd2c37bffec
Michael Volkmann <mvolkmann@tbz-pariv.de>
2019-07-01 14:24:00 +0200

    Cleanup imports

--- tbzuploader/utils.py
+++ tbzuploader/utils.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-import httplib
-import urlparse
-
 from future import standard_library
 
 standard_library.install_aliases()
+
+import httplib
+import urlparse
 import collections
 import datetime
 import logging
